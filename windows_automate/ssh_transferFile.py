e1390f8fe2fa1ade7af60570721e2b47223bd23e
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-18 19:28:01 +0600

    update 26

--- windows_automate/ssh_transferFile.py
+++ windows_automate/ssh_transferFile.py
@@ -9,8 +9,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname=hostname, username=username, password=password)
 
-# Run a file in certain location
-stdin, stdout, stderr = ssh.exec_command('python C:\\Users\\shiha\\Desktop\\uptime.py')
-print(stdout.read().decode())
-ssh.close()
-
+# Transfer file using sftp
+sftp = ssh.open_sftp()
+sftp.put(localpath='C:\\Users\\shiha\\OneDrive\\Desktop\\Python-main\\windows_automate\\uptime.py', remotepath='C:\\Users\\shiha\\Desktop\\uptime.py')
+sftp.close()
