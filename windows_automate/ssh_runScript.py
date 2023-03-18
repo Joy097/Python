8c15e69c981ff1eb5ca15a23a38a7fed4e9cca5b
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-18 18:57:53 +0600

    update 23

--- windows_automate/ssh_runScript.py
+++ windows_automate/ssh_runScript.py
@@ -11,9 +11,10 @@ ssh.connect(hostname=hostname, username=username, password=password)
 
 # Transfer file using sftp
 sftp = ssh.open_sftp()
-sftp.put(localpath='C:\\Users\\shiha\\OneDrive\\Desktop\\Python-main\\windows_automate\\uptime.py', remotepath='C:\\Users\\shiha\\Desktop\\scripts\\uptime.py')
+sftp.put(localpath='C:\\Users\\shiha\\OneDrive\\Desktop\\Python-main\\windows_automate\\uptime.py', remotepath='C:\\Users\\shiha\\Desktop\\uptime.py')
 sftp.close()
 
+# Run a file in certain location
 stdin, stdout, stderr = ssh.exec_command('python C:\\Users\\shiha\\Desktop\\uptime.py')
 print(stdout.read().decode())
 ssh.close()
