d06ce11821c502e9860b604047d0a1a023dcdae5
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-17 19:46:20 +0600

    update 129

--- Python Projects/todo/ruff.py
+++ Python Projects/todo/ruff.py
@@ -1,4 +1,6 @@
-list=['jabir','jaker','boroi']
+list=['2. jabir','3. jaker','boroi']
 for i in range(len(list)):
+    if ord(list[i][0]) >47 and ord(list[i][0])<58:
+        list[i] = list[i][3:]
     list[i] = str(i+1)+". "+list[i]
 print(list)
