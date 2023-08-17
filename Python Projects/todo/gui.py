f8faac2bee3df63f7b5359fdcca540b6253c89f3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-17 09:33:36 +0600

    update 8

--- Python Projects/todo/gui.py
+++ Python Projects/todo/gui.py
@@ -5,6 +5,8 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do")
 addButton = sg.Button("Add")
 
-window = sg.Window("My To-Do App",layout=[[label], [input_box,addButton]])
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box,addButton]],
+                   font=('Helvetica',20))
 window.read()
 window.close()
