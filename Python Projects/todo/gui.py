c49bed557031930f08a42eab7a81d1f81ce26146
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-17 17:55:01 +0600

    update 107

--- Python Projects/todo/gui.py
+++ Python Projects/todo/gui.py
@@ -41,7 +41,7 @@ while True:
                 todos = functions.read()
                 newtodo = value["todo"] +"\n"
                 
-                if value['todo'][0] == "":
+                if value["todos"] is None:
                     todos.append(newtodo)  
                 
                 else:                   #If anything is selected, add new task after that 
