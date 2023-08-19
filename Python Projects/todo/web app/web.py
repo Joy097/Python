e4ee09e9285ff625b2eff738deac10176e8f05e2
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-19 10:53:30 +0600

    update 11

--- Python Projects/todo/web app/web.py
+++ Python Projects/todo/web app/web.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import functions
 
+def add_task():
+    todo = st.session_state["newtodo"]
+    print(todo)
+
 todos=functions.read()
 st.title("My To-do app")
 st.subheader("subheader")
@@ -9,5 +13,6 @@ st.write("To write something")
 for todo in todos:
     st.checkbox(todo)
     
-input = st.text_input(label="",placeholder="Enter a task")
+st.text_input(label="",placeholder="Enter a task",
+                      on_change=add_task, key="newtodo")
 
