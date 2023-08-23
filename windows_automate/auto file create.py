093d872dad3b58eb2bbba626996b3cb21209066d
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-23 20:54:01 +0600

    update 10

--- windows_automate/auto file create.py
+++ windows_automate/auto file create.py
@@ -2,8 +2,8 @@ file_path = input("write the path :").strip()
 rounds = int(input("How many times? :").strip())
 count = 0
 for i in range(rounds):
-    path = f''
-    with open(file_path, "w") as file:
+    path = f'{file_path}file{count}'
+    with open(path, "w") as file:
         file.write("Hello, this is the content of the file.\n")
         
     count+=1
