c9c4a5803311b1523453009e4efe8a4811477974
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-23 21:00:22 +0600

    update 16

--- git_automate/git pull.py
+++ git_automate/git pull.py
@@ -6,3 +6,8 @@ repo_dir = input("Enter the path to the local git repository: ").strip()
 #os.chdir(repo_dir)
 files = os.listdir(repo_dir)
 
+for file in files:
+    file_path = os.path.join(repo_dir, file)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        print(f"Deleted {file_path}")
