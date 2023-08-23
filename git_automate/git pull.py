aa4b02e8a09616359e9469eda18b98647e2f55fe
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-23 21:02:28 +0600

    update 18

--- git_automate/git pull.py
+++ git_automate/git pull.py
@@ -3,14 +3,15 @@ import os
 import datetime
 
 def del_all():
-files = os.listdir(repo_dir)
+    files = os.listdir(repo_dir)
 
-for file in files:
-    file_path = os.path.join(repo_dir, file)
-    if os.path.isfile(file_path):
-        os.remove(file_path)
-        print(f"Deleted {file_path}")
+    for file in files:
+        file_path = os.path.join(repo_dir, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"Deleted {file_path}")
 
 repo_dir = input("Enter the path to the local git repository: ").strip()
-#os.chdir(repo_dir)
+os.chdir(repo_dir)
+question = "Do you want to dele"
 
