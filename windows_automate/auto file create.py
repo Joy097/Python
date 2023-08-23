d57d5a8646fcc005547776c0cf9dbd61a3513e5c
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-23 20:48:43 +0600

    update 5

--- windows_automate/auto file create.py
+++ windows_automate/auto file create.py
@@ -1,6 +1,4 @@
-file_path = input("write ")
+file_path = input("write the path :").strip()
 with open(file_path, "w") as file:
     file.write("Hello, this is the content of the file.\n")
-    file.write("This is another line of content.")
     
-print("File created and content written.")
