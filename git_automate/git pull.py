a8a0dac48cb3b93a8a47f73ad2d97179635d9c05
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-23 21:09:53 +0600

    update 25

--- git_automate/git pull.py
+++ git_automate/git pull.py
@@ -16,5 +16,5 @@ os.chdir(repo_dir)
 ask = input("Do you want to delete all files in the repository? (yes/n): ").strip()
 if ask == "yes":
     del_all()
-ask1 = input("Put the location to clone :").strip()
-cmd = ["git", "remote", "add", "origin", sshKey]
+git_repo = input("Put the location to clone :").strip()
+cmd = ["git", "clone",  git_repo]
