366642b7ac4efc2c53e99b84da988c360fa05380
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-24 19:22:39 +0600

    update 20

--- Python_Projects/web scrap/scrap1.py
+++ Python_Projects/web scrap/scrap1.py
@@ -3,5 +3,5 @@ import requests
 
 url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 page = requests.get(url)
-soup = BeautifulSoup(page.txt, 'html')
+soup = BeautifulSoup(page.text, 'html')
 print(soup)
