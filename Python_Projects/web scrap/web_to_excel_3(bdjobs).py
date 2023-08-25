04994d2459913730453c68271c2f8ff0bf089ad1
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-25 23:30:11 +0600

    update 4

--- Python_Projects/web scrap/web_to_excel_3(bdjobs).py
+++ Python_Projects/web scrap/web_to_excel_3(bdjobs).py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+url = 'https://bdjobs.com/career/careercouncil/CareerCounsellingCategory.asp'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html')
-table = soup.find_all('table')[1]
+table = soup.find_all(class = 'question-card')
 world_titles = table.find_all('th')
 world_titles = [title.text.strip() for title in world_titles]
 result=pd.DataFrame(columns=world_titles)
