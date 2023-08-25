03e21064a8a5b585e09ea855e84cddbe4eb28a9d
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-25 17:33:05 +0600

    update 51

--- Python_Projects/web scrap/webtable_to_excel_1(usb).py
+++ Python_Projects/web scrap/webtable_to_excel_1(usb).py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = "https://en.wikipedia.org/wiki/The_World%27s_Billionaires"
+url = "https://en.wikipedia.org/wiki/US-Bangla_Airlines"
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html')
 table = soup.find_all('table')[2]
