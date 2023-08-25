89fe01044fb910369f93d9df9134c19bba7daeec
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-25 16:53:56 +0600

    update 14

--- Python_Projects/web scrap/webtable_to_excel_1(usb).py
+++ Python_Projects/web scrap/webtable_to_excel_1(usb).py
@@ -5,5 +5,5 @@ import pandas as pd
 url = "https://en.wikipedia.org/wiki/US-Bangla_Airlines"
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html')
-table = soup.find_all('table')
+table = soup.find_all('table')[0]
 print(table)
