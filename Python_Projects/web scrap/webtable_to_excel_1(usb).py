7962ff3d2bcfce5e9efce18368c36bb32d7e9095
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-25 17:29:54 +0600

    update 48

--- Python_Projects/web scrap/webtable_to_excel_1(usb).py
+++ Python_Projects/web scrap/webtable_to_excel_1(usb).py
@@ -10,8 +10,8 @@ header = table.find_all('th')
 hlist = [head.text.strip() for head in header]
 daf = pd.DataFrame(columns=hlist)
 print(daf)
-
 rows = table.find_all('tr')
+
 for row in rows:
     elements = row.find_all('td')
     elist = [elem.text.strip() for elem in elements]
