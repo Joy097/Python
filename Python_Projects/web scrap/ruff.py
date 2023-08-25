008fb1a2f8a2ea3af51c4ce1e2743703918f73a5
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-26 00:20:57 +0600

    update 52

--- Python_Projects/web scrap/ruff.py
+++ Python_Projects/web scrap/ruff.py
@@ -6,11 +6,11 @@ url = "https://bdjobs.com/career/careercouncil/CareerCounsellingCategory.asp"
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html')
 table = soup.find('div',class_='question-card')
-info = table.find_all('ul')[0]
-
+info = table.find_all('ul')
+name = info.find_all('li')[0]
 #hlist = [head.text.strip() for head in header]
 #daf = pd.DataFrame(columns=hlist)
-print(header)
+print(name)
 
 '''
 rows = table.find_all('tr')
