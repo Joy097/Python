b0ef14ec710f2efc764ec0016bd3fccc16d5c2b0
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-25 23:25:57 +0600

    update 0

---
+++ Python_Projects/web scrap/web_to_excel_3(bdjobs).py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html')
+table = soup.find_all('table')[1]
+world_titles = table.find_all('th')
+world_titles = [title.text.strip() for title in world_titles]
+result=pd.DataFrame(columns=world_titles)
+rows = table.find_all('tr')
+
+'''
+for row in rows[1:]:
+    row_data = row.find_all('td')
+    row_data = [list.text.strip() for list in row_data]
+    length = len(result)
+    result.loc[length] = row_data
+print(result)
+result.to_csv(r'C:\\Users\\shiha\\OneDrive\Desktop\\Python-main\\companies.csv',index=False)
+
+'''
