16cf081342fc403a60de540dbcccc2093d09332b
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-26 00:04:02 +0600

    update 36

--- Python_Projects/web scrap/web_to_excel_3(bdjobs).py
+++ Python_Projects/web scrap/web_to_excel_3(bdjobs).py
@@ -4,7 +4,7 @@ import pandas as pd
 
 url = 'https://bdjobs.com/career/careercouncil/CareerCounsellingCategory.asp'
 page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(page.text, 'html.parser')  # Use 'html.parser' here
 table = soup.find_all(class_='question-card')
 
 # Create an empty list to store the extracted data
@@ -12,12 +12,9 @@ reply_list = []
 
 # Loop through the 'question-card' elements
 for part in table[0]:
-    if isinstance(part, str):
-        reply_list.append(part.strip())  # Append the string content
-    else:
-        replies = part.find_all('p')
-        for reply in replies:
-            reply_list.append(reply.get_text(strip=True))
+    replies = part.find_all('p')
+    for reply in replies:
+        reply_list.append(reply.get_text(strip=True))
 
 # Create a DataFrame using pandas
 df = pd.DataFrame(reply_list, columns=['Replies'])
