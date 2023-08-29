911e1592cdd25ded4c6c6e8b1512ce30778f3c7d
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-29 15:45:29 +0600

    update 218

--- Python_Projects/web scrap/rough.py
+++ Python_Projects/web scrap/rough.py
@@ -1,6 +1,7 @@
 import pywhatkit
 from datetime import datetime
 current_time = datetime.now().time()
-hour = cu
+hour = current_time.strftime('%H')
+minute = current_time.strftime('%M')
 
-pywhatkit.sendwhatmsg('+8801959842041','Happy Birthday',15,41)
+pywhatkit.sendwhatmsg('+8801959842041','Happy Birthday',hour,41)
