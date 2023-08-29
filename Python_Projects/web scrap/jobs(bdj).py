063fd2de7d4d4a0e4705bbaf928f795aa24b64f3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-29 15:59:26 +0600

    update 231

--- Python_Projects/web scrap/jobs(bdj).py
+++ Python_Projects/web scrap/jobs(bdj).py
@@ -40,5 +40,6 @@ if 'Na' in exp:
     indx=exp.index('Na')
     str1=f'There is a {job_ttl[indx]} job for you with no experience at {comp[indx]} inside {loc[indx]}!!'
 
+pywhatkit.sendwhatmsg('+8801959842041','Happy Birthday',hour,minute+1)
 
 
