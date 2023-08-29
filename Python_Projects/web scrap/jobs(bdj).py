0e67ef30d658eb5209dc99e6c3977d9ddaba0fdd
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-29 16:08:02 +0600

    update 239

--- Python_Projects/web scrap/jobs(bdj).py
+++ Python_Projects/web scrap/jobs(bdj).py
@@ -39,7 +39,7 @@ str1=''
 if 'Na' in exp:
     indx=exp.index('Na')
     str1=f'There is a {job_ttl[indx]} job for you with no experience at {comp[indx]} inside {loc[indx]}!!'
-
-pywhatkit.sendwhatmsg('+8801959842041',str(str1),hour,minute+2)
+print(exp)
+#pywhatkit.sendwhatmsg('+8801959842041',str(str1),hour,minute+2)
 
 
