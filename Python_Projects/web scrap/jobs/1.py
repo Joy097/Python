7573c45b2ddd94d50f8e2aad2453d416ab0c57b4
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-30 21:23:16 +0600

    update 3

--- Python_Projects/web scrap/jobs/1.py
+++ Python_Projects/web scrap/jobs/1.py
@@ -9,18 +9,23 @@ import pywhatkit
 from datetime import datetime
 
 # Create a new instance of the Chrome browser
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 
 # URL of the webpage with pagination
 url = "https://jobs.bdjobs.com/JobSearch.asp?icatId=&requestType=deadline"
 
 # Open the webpage
+page = requests.get(url)
+soup = BeautifulSoup(page.text,'html')
+table = soup.find_all('div',class_='norm-jobs-wrapper')
+
+
+'''
 driver.get(url)
-
-
 # Locate and click the "Next" button or pagination link
 next_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Next')]")
 next_button.click()
 
 # Wait for a moment for the page to load
 time.sleep(10)
+'''
