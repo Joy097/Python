59fa1ca1e54f1b658025b773719162e4ae7e6d44
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-31 01:21:33 +0600

    update 50

--- Python_Projects/web scrap/jobs/rf.py
+++ Python_Projects/web scrap/jobs/rf.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 current_time = datetime.now().time()
-hour = int(current_time.strftime('%H'))
-minute = int(current_time.strftime('%M'))
+time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+print(time)
