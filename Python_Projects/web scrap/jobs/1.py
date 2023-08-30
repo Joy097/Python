8da2cf5780321dffdc3d82440e9ef576b0dafdaa
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-30 21:08:03 +0600

    update 0

---
+++ Python_Projects/web scrap/jobs/1.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import pywhatkit
+from datetime import datetime
+
+# Create a new instance of the Chrome browser
+driver = webdriver.Chrome()
+
+# URL of the webpage with pagination
+url = "https://jobs.bdjobs.com/JobSearch.asp?icatId=&requestType=deadline"
+
+# Open the webpage
+driver.get(url)
+
+# Locate and click the "Next" button or pagination link
+next_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Next')]")
+next_button.click()
+
+# Wait for a moment for the page to load
+time.sleep(10)
