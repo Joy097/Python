f89fd3fba273480fa9944c01cb70335616b499f2
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-31 01:36:45 +0600

    update 53

--- Python_Projects/web scrap/jobs/rf.py
+++ Python_Projects/web scrap/jobs/rf.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 import pandas as pd
+job_ttl=[]
+comp = []
+loc=[]
+edu = []
+exp = []
+date = []
 current_time = datetime.now()
-time = current_time.strftime('%Y-%m-%d %H:%M:%S')
-data = {'Job Title':'job_ttl', 'Company':'comp', 'Location':'loc', 'Education':'edu', 'Experience':'exp', 'Deadline':'date'}
+time = current_time.strftime('%d-%m_%H:%M')
+data = {'Job Title':job_ttl, 'Company':comp, 'Location':loc, 'Education':edu, 'Experience':exp, 'Deadline':date}
 df = pd.DataFrame(data)
-df.to_csv(r'bdjobs{time}.csv',index=False,encoding='utf-8')
+txt = f'bdjobs{time}.csv'
+df.to_csv(txt,index=False,encoding='utf-8')
