4082d7138135aec7dbfa5032525dae3146fd1387
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-30 21:48:35 +0600

    update 8

--- Python_Projects/web scrap/jobs/1.py
+++ Python_Projects/web scrap/jobs/1.py
@@ -18,9 +18,10 @@ url = "https://jobs.bdjobs.com/JobSearch.asp?icatId=&requestType=deadline"
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html')
 table = soup.find_all('div',id='bottomPagging')
-for i in table[-1:]:    
+table = soup.find_all('li')
+for i in table:    
     
-    print(table)
+    print(i)
 
 
 '''
