401e1d13fe6f13757b61171d698a6639db61cbb8
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-30 22:44:20 +0600

    update 19

--- Python_Projects/web scrap/jobs/1.py
+++ Python_Projects/web scrap/jobs/1.py
@@ -21,14 +21,25 @@ table = soup.find('div',id='bottomPagging')
 table = table.find_all('li')
 lst_pg = (i.text.strip() for i in table[5:]).__next__()  
 
+
+def get_page_html(driver):
+    return driver.execute_script("return document.documentElement.outerHTML;")
+
+# Get and print the current page's HTML
+
+print(current_page_html)
+count=0
 driver.get(url)  
 for i in range(int(lst_pg[3:])):
     current_page_url = driver.current_url
     print(current_page_url)
+    current_page_html = get_page_html(driver)
+    
     # Locate and click the "Next" button or pagination link
     next_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Next')]")
     next_button.click()
 
     # Wait for a moment for the page to load
     time.sleep(10)
+    count+=1
 
