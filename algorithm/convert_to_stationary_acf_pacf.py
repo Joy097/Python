e7ff7301cf9dc70d703226cdc6fd1fc79a531953
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 16:59:54 +0600

    update 41

--- algorithm/convert_to_stationary_acf_pacf.py
+++ algorithm/convert_to_stationary_acf_pacf.py
@@ -1,4 +1,15 @@
 import yfinance as yf
+import matplotlib as plt
+import pandas as pd
+
 ticker_symbol = 'SPY'
 tickerdata = yf.Ticker(ticker_symbol)
-tickerDf = tickerdata.history(period='1d', start='2015-1-1', end='2020-1-1')
+tickerDf = tickerdata.history(period='1d', start='2018-1-1', end='2020-1-1')
+tickerDf = tickerDf[['Close']]
+
+plt.figure(figsize=(10,4))
+plt.plot(tickerDf.Close)
+plt.title('Stock Price over Time (%s)'%ticker_symbol, fontsize=20)
+plt.ylabel('Price', fontsize=16)
+for year in range(2015,2021):
+    plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
