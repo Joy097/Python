24b8b7b67ee2eae89db61cb12f40368f98616078
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 16:41:50 +0600

    update 24

--- algorithm/acf_pacf.py
+++ algorithm/acf_pacf.py
@@ -19,4 +19,6 @@ plt.title('Ice-cream production', fontsize=20)
 plt.ylabel('Production', fontsize = 16)
 for year in range(2011,2021):
     plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
+#ACF
+acf_plot = plot_acf(df_ice_cream.production, lags=100)
     
