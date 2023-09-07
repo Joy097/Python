a51a377d2b777c98779a275ddc0e4ad445f84194
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 16:24:53 +0600

    update 8

--- algorithm/acf_pacf.py
+++ algorithm/acf_pacf.py
@@ -5,10 +5,11 @@ from pandas.plotting import register_matplotlib_converters
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 register_matplotlib_converters()
 df_ice_cream = pd.read_csv('ice_cream.csv')
-print(df_ice_cream.head())
 
-df_ice_cream.renmae(columns={'DATE':'date','IPN31152N':'production'}, inplace=True)
+
+df_ice_cream.rename(columns={'DATE':'date','IPN31152N':'production'}, inplace=True)
 df_ice_cream['date'] = pd.to_datetime(df_ice_cream.date)
 df_ice_cream.set_index('date', inplace=True)
 start_date = pd.to_datetime('2010-01-01')
-df_ice_cream = df_ice_cream[s]
+df_ice_cream = df_ice_cream[start_date:]
+print(df_ice_cream.head())
