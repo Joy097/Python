0c172529cac7ddaff5e2e21eb1ec6afb1189f303
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 16:18:28 +0600

    update 2

---
+++ algorithm/acf_pacf.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+register_matplotlib_converters()
+df_ice_cream = pd.read_csv('ice_cream.csv')
+print(df_ice_cream.head())
+
