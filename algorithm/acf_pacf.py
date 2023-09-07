52b0fe02da649143ae620ab52ead93e44f97bf89
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 16:23:49 +0600

    update 7

--- algorithm/acf_pacf.py
+++ algorithm/acf_pacf.py
@@ -10,3 +10,5 @@ print(df_ice_cream.head())
 df_ice_cream.renmae(columns={'DATE':'date','IPN31152N':'production'}, inplace=True)
 df_ice_cream['date'] = pd.to_datetime(df_ice_cream.date)
 df_ice_cream.set_index('date', inplace=True)
+start_date = pd.to_datetime('2010-01-01')
+df_ice_cream = df_ice_cream[s]
