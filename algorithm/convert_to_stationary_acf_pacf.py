36906f0ea68ea7c0b29feb930b0b9e22e3827bdc
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 17:05:15 +0600

    update 46

--- algorithm/convert_to_stationary_acf_pacf.py
+++ algorithm/convert_to_stationary_acf_pacf.py
@@ -11,10 +11,4 @@ plt.figure(figsize=(10,4))
 plt.plot(tickerDf.Close)
 plt.title('Stock Price over Time (%s)'%ticker_symbol, fontsize=20)
 plt.ylabel('Price', fontsize=16)
-for year in range(2018,2021):
-    plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
-    plt.axvline(pd.to_datetime(str(year)+'-01-04'), color='k', linestyle='--', alpha=0.2)
-    plt.axvline(pd.to_datetime(str(year)+'-01-07'), color='k', linestyle='--', alpha=0.2)
-    plt.axvline(pd.to_datetime(str(year)+'-01-10'), color='k', linestyle='--', alpha=0.2)
-
 plt.savefig('plot1.png')
