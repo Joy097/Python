5ee87a72ad158485929410f461e5f91dc2cca901
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-07 17:44:39 +0600

    update 83

--- algorithm/AR.py
+++ algorithm/AR.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import datetime
-from datetime import datetime
+from datetime import timedelta
 from pandas.plotting import register_matplotlib_converters
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima_model import ARMA
@@ -14,7 +14,7 @@ def parser(s):
 production_ice_cream = pd.read_csv('ice_cream.csv', parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 production_ice_cream.rename('production', inplace=True)
 print(production_ice_cream)
-production_ice_cream=production_ice_cream.asfreq(pd.infer_ferq(production_ice_cream.index))
+production_ice_cream=production_ice_cream.asfreq(pd.infer_freq(production_ice_cream.index))
 
 start_date = pd.to_datetime('2010-01-01')
 production_ice_cream = production_ice_cream[start_date:]
