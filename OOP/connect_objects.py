dd558bcbd944fc32ce0e9d3e9bb298f01e435775
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-11 19:43:03 +0600

    update 61

--- OOP/connect_objects.py
+++ OOP/connect_objects.py
@@ -5,3 +5,13 @@ class husband:
     
     def marry(self,wife):
         self.wife = wife
+        
+class wife:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def marry(self,husband):
+        self.husband = husband
+        
+husb
