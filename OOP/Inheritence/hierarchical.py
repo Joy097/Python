b7b92cd59c52d166347f51e7880542b097300e00
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-11 18:55:22 +0600

    update 16

--- OOP/Inheritence/hierarchical.py
+++ OOP/Inheritence/hierarchical.py
@@ -7,4 +7,13 @@ class father:
         self.work = True
         
 class son1(father):
-    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.work= False
+class son2(father):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.work= False
+        
