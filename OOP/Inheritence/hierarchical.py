e4a70d8470c62c4f78ff8240e56888fb714dba2b
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-11 19:16:33 +0600

    update 36

--- OOP/Inheritence/hierarchical.py
+++ OOP/Inheritence/hierarchical.py
@@ -11,13 +11,14 @@ class son1(father):
         self.name = name
         self.age = age
         self.working= False
-        super().__init__()
+        super().__init__(fname,fage)
 class son2(father):
-    def __init__(self, name, age):
+    def __init__(self, name, age, fname, fage):
         self.name = name
         self.age = age
         self.working= False
+        super().__init__(fname,fage)
         
-zaber = son1('zaber',34)
+zaber = son1('zaber',34, 'jaker')
 zaber.work()
 print(zaber.working)
