e90d9c07a2f3fc01058f0bf707ed41712f8a7420
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-12 12:03:06 +0600

    update 3

--- algorithm/linked list/single linkedList.py
+++ algorithm/linked list/single linkedList.py
@@ -10,9 +10,22 @@ class LinkedList:
         newNode = Node(data,self.head)
         self.head = newNode
             
+    def print(self):
+        if(self.head):
+            head = self.head
+            str = ''
+            while(head):
+                str = head + '-->'
+                head = head.next
+        else:
+            return "List is empty"
         
-        
+                
 LL = LinkedList()
 LL.insertFirst(2)
 LL.insertFirst(3)
+LL.insertFirst(4)
+LL.insertFirst(5)
+
+LL.print
         
