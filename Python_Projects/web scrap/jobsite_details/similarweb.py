d08306730973bd2a00ce9d2978012acf38940928
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-24 14:31:08 +0600

    update 6

--- Python_Projects/web scrap/jobsite_details/similarweb.py
+++ Python_Projects/web scrap/jobsite_details/similarweb.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 
-url = f"https://www.similarweb.com/website/bdjobs.com"
+url = f"https://www.similarweb.com/website/bdjobs.com/#overview"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 print(soup)
